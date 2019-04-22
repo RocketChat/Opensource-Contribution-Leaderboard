0281180716a53446d379c5c75e8691228f6455dd
lolimay <lolimay@lolimay.cn>
2019-04-22 19:18:01 +0800

    Feat: sort by merged PRs

--- src/index.js
+++ src/index.js
@@ -6,13 +6,46 @@ axios.get('/assets/data/data.json')
     .then( res => {
         const table = document.querySelector('table')
         const data = res.data
-        Object.keys(data).forEach( contributor => {
+        const list = Object.keys(data)
+        let contributors = []
+        list.forEach( username => {
+            contributors.push({
+                username,
+                mergedPRsNumber: data[username].mergedPRsNumber,
+                openPRsNumber: data[username].openPRsNumber,
+                issuesNumber: data[username].issuesNumber
+            })
+        })
+        console.log(contributors)
+        contributors = contributors.sort( (a, b) => {
+            if ( a.mergedPRsNumber < b.mergedPRsNumber ){
+                return 1;
+            }
+            if ( a.mergedPRsNumber > b.mergedPRsNumber ){
+                return -1;
+            }
+            if ( a.openPRsNumber < b.openPRsNumber ){
+                return 1;
+            }
+            if ( a.openPRsNumber > b.openPRsNumber ){
+                return -1;
+            }
+            if ( a.openPRsNumber < b.openPRsNumber ){
+                return 1;
+            }
+            if ( a.openPRsNumber > b.openPRsNumber ){
+                return -1;
+            }
+            return 0;
+        })
+        console.log(contributors)
+        contributors.forEach( contributor => {
             const tr = document.createElement('tr')
             
             // avatar
             const tdAvatar = document.createElement('td')
             const avatar = document.createElement('img')
-            avatar.src = data[contributor].avatarUrl
+            avatar.src = data[contributor.username].avatarUrl
             avatar.height = '42'
             avatar.width = '42'
             tdAvatar.appendChild(avatar)
@@ -21,8 +54,8 @@ axios.get('/assets/data/data.json')
             // username
             const tdUsername = document.createElement('td')
             const username = document.createElement('a')
-            username.href = data[contributor].home
-            username.innerText = contributor
+            username.href = data[contributor.username].home
+            username.innerText = contributor.username
             tdUsername.appendChild(username)
             tr.appendChild(tdUsername)
 
@@ -32,24 +65,24 @@ axios.get('/assets/data/data.json')
             // Open PRs
             const tdOpenPRs = document.createElement('td')
             const openPRs = document.createElement('a')
-            openPRs.href = data[contributor].openPRsLink
-            openPRs.innerText = data[contributor].openPRsNumber
+            openPRs.href = data[contributor.username].openPRsLink
+            openPRs.innerText = data[contributor.username].openPRsNumber
             tdOpenPRs.appendChild(openPRs)
             tr.appendChild(tdOpenPRs)
 
             // Merged PRs
             const tdMergedPRs = document.createElement('td')
             const mergedPRs = document.createElement('a')
-            mergedPRs.href = data[contributor].mergedPRsLink
-            mergedPRs.innerText = data[contributor].mergedPRsNumber
+            mergedPRs.href = data[contributor.username].mergedPRsLink
+            mergedPRs.innerText = data[contributor.username].mergedPRsNumber
             tdMergedPRs.appendChild(mergedPRs)
             tr.appendChild(tdMergedPRs)
 
             // Issues
             const tdIssues = document.createElement('td')
             const issues = document.createElement('a')
-            issues.href = data[contributor].issuesLink
-            issues.innerText = data[contributor].issuesNumber
+            issues.href = data[contributor.username].issuesLink
+            issues.innerText = data[contributor.username].issuesNumber
             tdIssues.appendChild(issues)
             tr.appendChild(tdIssues)
 
