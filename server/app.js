e5d376a363d95cf12a91eab71fc65a510a8983fc
lolimay <lolimay@lolimay.cn>
2019-04-22 11:42:47 +0800

    initial commit

---
+++ server/app.js
@@ -0,0 +1,7 @@
+const request = require('request');
+
+request('http://www.baidu.com', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
