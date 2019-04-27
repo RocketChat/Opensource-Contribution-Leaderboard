e7ef19b643a4f5fa78827bd50b6222c35e646680
lolimay <lolimay@lolimay.cn>
2019-04-27 21:29:43 +0800

    Implement delay hot load

--- src/server/refresh.js
+++ src/server/refresh.js
@@ -6,11 +6,12 @@ const fs = require('fs')
 const dataBasePath = '../assets/data'
 const dataPath = '../assets/data/data.json'
 const logPath = '../assets/data/log.json'
-const { delay } = jsonfile.readFileSync('./config.json')
+const configPath = './config.json'
 
 let interval = 150
 let dataBuffer = {}
 let logBuffer = {}
+let delay = jsonfile.readFileSync(configPath).delay
 
 if (!fs.existsSync(dataBasePath)) {
     fs.mkdirSync(dataBasePath)
@@ -25,7 +26,7 @@ if (fs.existsSync(logPath)) {
 }
 
 async function getAllContributorsInfo() {
-    const Config = jsonfile.readFileSync('./config.json')
+    const Config = jsonfile.readFileSync(configPath)
     const organization = Config.organization
     const contributors = Config.contributors
 
@@ -37,8 +38,11 @@ async function getAllContributorsInfo() {
     Promise.mapSeries(contributors, async contributor => {
 
         await Promise.delay(delay * 1000)
+        console.log(delay)
                 
         API.getContributorInfo(organization, contributor).then( res => {
+            delay = jsonfile.readFileSync(configPath).delay // update delay
+
             if (res.avatarUrl !== '' && res.issuesNumber !== -1 && res.mergedPRsNumber !== -1 && res.openPRsNumber != -1) {
                 dataBuffer[`${contributor}`] = res
                 console.log(res)
