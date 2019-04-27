86902852e962e6e672916f983e25c3d7bcf82cdd
lolimay <lolimay@lolimay.cn>
2019-04-27 15:34:30 +0800

    Add tests/fetxh.js for admindata generation

---
+++ src/server/tests/fetch.js
@@ -0,0 +1,19 @@
+const API = require('../util/API')
+const jsonfile = require('jsonfile')
+
+const contributors = jsonfile.readFileSync('../../assets/data/data.json')
+const contributorsList = []
+
+for (let contributor in contributors) {
+
+    const username = contributor
+    const { avatarUrl } = contributors[contributor]
+
+    contributorsList.push( {username, avatarUrl} )
+}
+
+jsonfile.writeFileSync('../admindata.json', contributorsList, { spaces: 2 }, err => {
+    if (err) {
+        console.log(err)
+    }
+})
