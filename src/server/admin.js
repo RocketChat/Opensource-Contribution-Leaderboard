f35cae1a64f4fe49d881a5e3dc1e502524b0e502
lolimay <lolimay@lolimay.cn>
2019-04-27 19:57:19 +0800

    Complete Admin.js

--- src/server/admin.js
+++ src/server/admin.js
@@ -6,7 +6,9 @@ const API = require('./util/API')
 const fs = require('fs')
 
 const port = 52050
+const configPath = './config.json'
 const admindataPath = './admindata.json'
+const dataPath = '../assets/data/data.json'
 
 if (!fs.existsSync(admindataPath)) {
     jsonfile.writeFileSync(admindataPath, [] )
@@ -15,16 +17,16 @@ if (!fs.existsSync(admindataPath)) {
 const server = http.createServer( (req, res) => {
     const route = url.parse(req.url).pathname
     const params = url.parse(req.url, true).query
+    const { adminPassword } = jsonfile.readFileSync(configPath)
 
     switch (route) {
-        
         case '/login':
-            const { adminPassword, delay, contributors } = jsonfile.readFileSync('./config.json')
+            let { delay, contributors } = jsonfile.readFileSync(configPath)
             const contributorsList = []
 
             Util.post(req, async params => {
                 const { token } = params
-                if (token == adminPassword) {
+                if (token === adminPassword) {
                     await Promise.all(contributors.map( async contributor => {
                         const admindata = jsonfile.readFileSync('./admindata.json')
                         const existContributor = findContributor(contributor, admindata)
@@ -33,7 +35,6 @@ const server = http.createServer( (req, res) => {
                             contributorsList.push(existContributor)
                         } else {
                             const avatarUrl = await API.getContributorAvatar(contributor)
-                            console.log(avatarUrl)
                             if(avatarUrl !== '') {
                                 contributorsList.push({
                                     username: contributor,
@@ -50,6 +51,90 @@ const server = http.createServer( (req, res) => {
                 }
             })
            break
+        case '/setInterval':
+            Util.post(req, params => {
+                const { token, interval } = params
+
+                if (token !== adminPassword) {
+                    res.end(JSON.stringify({ message: 'Authentication failed' }))
+                } else {
+                    // set delay in config.json
+                    const Config = jsonfile.readFileSync(configPath)
+                    Config.delay = interval
+                    jsonfile.writeFileSync(configPath, Config, { spaces: 2 })
+
+                    res.end(JSON.stringify({ message: 'Success' }))
+                }
+            })
+            break
+        case '/remove':
+            Util.post(req, params => {
+                const { token, username } = params
+
+                if (token !== adminPassword) {
+                    res.end(JSON.stringify({ message: 'Authentication failed' }))
+                } else {
+                    const Config = jsonfile.readFileSync(configPath)
+                    // Remove this contributor in config.json
+                    Config.contributors.forEach( (contributor, index, object) => {
+                        if (contributor == username) {
+                            object.splice(index, 1)
+                        }
+                    })
+                    jsonfile.writeFileSync(configPath, Config, { spaces:2 })
+
+                    // Remove this contributor in the data.json
+                    const data = jsonfile.readFileSync(dataPath)
+                    delete data[username]
+                    jsonfile.writeFileSync(dataPath, data, { spaces: 2 })
+
+                    res.end(JSON.stringify({ message: 'Success' }))
+                }
+            })
+            break
+        case '/add':
+            Util.post(req, params => {
+                const { token, username } = params
+
+                if (token !== adminPassword) {
+                    res.end(JSON.stringify({ message: 'Authentication failed' }))
+                } else {
+                    const Config = jsonfile.readFileSync(configPath)
+
+                    if (Config.contributors.includes(username)) {
+                        res.end(JSON.stringify({ message: `${username} aready exists` }))
+                        return
+                    }
+
+                    // Add this contributor in config.json
+                    Config.contributors.push(username)
+                    jsonfile.writeFileSync(configPath, Config, { spaces:2 })
+
+                    // Add this contributor in the data.json
+                    const data = jsonfile.readFileSync(dataPath)
+                    API.getContributorInfo(Config.organization, username).then( result => {
+                        if (result.avatarUrl !== '' && result.issuesNumber !== -1 && result.mergedPRsNumber !== -1 && result.openPRsNumber != -1) {
+                            data[`${username}`] = result
+                            // Update contributors infomation
+                            jsonfile.writeFile(dataPath, data, { spaces: 2 }, (err) => {
+                                if (err) console.error(err)
+                            })
+                        }
+                    })
+
+                    API.getContributorAvatar(username).then( result => {
+                        if (result === '') {
+                            res.end(JSON.stringify({ message: 'Not found' }))
+                        } else {
+                            res.end(JSON.stringify({
+                                message: 'Success',
+                                avatarUrl: result
+                        }))
+                        }
+                    })
+                }
+            })
+            break
         default:
             res.end('Hello World!')
             break
