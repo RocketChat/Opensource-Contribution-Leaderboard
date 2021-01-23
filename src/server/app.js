796994c8e9679bc5282917ba7a2c7fab3a2f8171
Rohan Lekhwani <rohanlekhwani@ymail.com>
2021-01-23 10:16:53 +0530

    Fixes flat compatibility bug by using a custom flat function

--- src/server/app.js
+++ src/server/app.js
@@ -26,6 +26,14 @@ const websocketProxyOption = {
     changeOrigin: true
 }
 
+Object.defineProperty(Array.prototype, 'flat', {
+    value: function(depth = 1) {
+        return this.reduce(function (flat, toFlatten) {
+            return flat.concat((Array.isArray(toFlatten) && (depth>1)) ? toFlatten.flat(depth-1) : toFlatten)
+        }, [])
+    }
+})
+
 if (process.env.NODE_ENV !== 'development') {
     app.use('/api', proxy(proxyOption))
     app.all('/server/*', (req, res, next) => {
